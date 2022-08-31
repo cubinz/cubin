e8f23268b14896fc7c61e2c3b551a17d40d77b43
cubinz <111338817+cubinz@users.noreply.github.com>
2022-09-01 03:01:39 +0800

    Delete utils directory

--- utils/application.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if test -z "$COMMAND"
-then
-    wine explorer /desktop=application,${DISPLAY_WIDTH}x${DISPLAY_HEIGHT} ${APPLICATION}
-else
-    eval $COMMAND
-fi
--- utils/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp /root/utils/index.html /usr/share/novnc/index.html
-sed -i "s/\${HOSTNAME}/${HOSTNAME}/g" /usr/share/novnc/index.html
-
-supervisord -c /root/utils/supervisord.conf
